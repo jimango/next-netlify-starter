e6e5564208a5a43cf2386946ab2d3086df953dd5
jimango <jonathan.hol@gmail.com>
2023-01-13 08:37:06 +0100

    cahnges to info

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Quark 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Our website is being rebuildt<code>until then check our QuarkToken.io</code>
         </p>
       </main>
 
